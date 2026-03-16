9796c409552f2f9afe6a36a9db07992996084573
Leonardo Cunha <leo.francasp97@gmail.com>
2026-03-16 20:54:53 -0300

    Anotações até aula 40

---
+++ web/cypress/e2e/studio.cy.js
@@ -0,0 +1,15 @@
+describe("Studio", () => {
+  it("Exemplo do Cypress Studio", () => {
+    cy.visit("https://example.cypress.io");
+    cy.get("h1").should("be.visible").and("have.text", "Kitchen Sink");
+  });
+
+  it("Deve logar com sucesso", function () {
+    cy.visit("http://localhost:3000");
+
+    cy.get("#email").type("papito@webdojo.com");
+    cy.get("#password").type("katana123");
+    cy.contains("button", "Entrar").click();
+    cy.get('[data-cy="user-name"]').should("have.text", "Fernando Papito");
+  });
+});
